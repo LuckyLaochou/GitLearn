74470d20d03ae7ccfccef939805b5dc08a00826d
LuckyLaochou <yzllaochou@gmail.com>
2021-05-04 12:39:55 +0800

    second commit

---
+++ src/cn/laochou/service/GitService.java
@@ -0,0 +1,21 @@
+package cn.laochou.service;
+
+public class GitService {
+
+    /**
+     * 提交项目
+     * @return 返回提交结果
+     */
+    public boolean commitProject() {
+        return true;
+    }
+
+    /**
+     * 克隆项目
+     * @return 返回克隆结果
+     */
+    public boolean cloneProject() {
+        return true;
+    }
+    
+}
