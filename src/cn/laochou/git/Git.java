ecdf92ab06ae98e3fae5d5ba5594f06dc508611f
LuckyLaochou <yzllaochou@gmail.com>
2021-05-04 12:00:14 +0800

    first commit

---
+++ src/cn/laochou/git/Git.java
@@ -0,0 +1,47 @@
+package cn.laochou.git;
+
+/**
+ * git 网站属性
+ */
+public class Git {
+
+    private String url;
+
+    private String name;
+
+    private String ip;
+
+    private String owner;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
